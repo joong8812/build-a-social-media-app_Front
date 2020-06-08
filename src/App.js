812b7c2457ffc81c0a5ce6f88d7528c19a984e5d
joong8812 <joong8812@gmail.com>
2020-06-07 23:21:26 -0400

    User Page

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ import AuthRoute from "./util/AuthRoute";
 import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
+import user from "./pages/user"
+
 import axios from "axios";
 
 const theme = createMuiTheme(themeFile);
@@ -54,6 +56,7 @@ class App extends Component {
                   path="/signup"
                   component={signup}
                 />
+                <Route exact path="/users/:handle" component={user} />
               </Switch>
             </div>
           </Router>
